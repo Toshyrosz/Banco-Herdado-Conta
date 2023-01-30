d27956898679ee9c66844b2565d597165fea0722
Toshyrosz <danielfs1989@gmail.com>
2023-01-30 15:34:17 -0300

    Classe generica array

---
+++ Banco-Herdado-Conta/src/br/com/bytebank/banco/teste/TesteArrayDeString.java
@@ -0,0 +1,12 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteArrayDeString {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0  ; i<args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+
+}
--- Banco-Herdado-Conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ Banco-Herdado-Conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,25 +1,33 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
-		ContaCorrente[] contas = new ContaCorrente [5];
+		Object[] referencias = new Object [5];
+		
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
-		contas[0]=cc1;
-		ContaCorrente cc2 = new ContaCorrente(22, 12);
-		contas[1]=cc2;
-		ContaCorrente cc3 = new ContaCorrente(22, 13);
-		contas[2]=cc3;
+		referencias[0]=cc1;
+		ContaPoupanca cc2 = new ContaPoupanca(22, 12);
+		referencias[1]=cc2;
+		ContaCorrente cc3 = new ContaCorrente(22, 13); 
+		referencias[2]=cc3;
 		ContaCorrente cc4 = new ContaCorrente(22, 14);
-		contas[3]=cc4;
+		referencias[3]=cc4;
 		ContaCorrente cc5 = new ContaCorrente(22, 15);
-		contas[4]=cc5;
+		referencias[4]=cc5;
 		
-		System.out.println(contas[1].getNumero());
+		Cliente cliente = new Cliente();
+		referencias[2]=cliente;
 		
-		ContaCorrente ref= contas[1];
+		//System.out.println(contas[1].getNumero());
+		
+		//Object referenciaGenerica =contas[1];
+		
+		ContaPoupanca ref = (ContaPoupanca) referencias[1]; //type cast
 		System.out.println(ref.getNumero());
 	}
 
